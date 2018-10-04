463c7da80440a798d52ccc26155afab47e2ce647
A01747290 <42151769+DpalmeITESM@users.noreply.github.com>
2018-10-04 12:26:45 -0500

    Update Mision05.py

--- Mision05.py
+++ Mision05.py
@@ -45,9 +45,11 @@ def dibujarA():
     for n in range(40):
         pygame.draw.circle(win, negro, (400, 400), (n+1) * 10, 1)
 
+    pygame.display.update()
+
     run = True
 
-    reloj = pygame.time
+    reloj = pygame.time.Clock()
 
     # Lógica de pygame que básicamente es la función main pero en tiempo real
     while run:
@@ -58,8 +60,6 @@ def dibujarA():
                 run = False
                 pygame.quit()
                 main()
-
-    pygame.display.update()
 
 
 # Dibuja del punto más grande de un eje al más pequeño del otro para todos los cuadrantes
@@ -83,9 +83,11 @@ def dibujarB():
         pygame.draw.line(win, colorAleatorio(), (400 - n, 400), (400, 800 - n), 1)
         pygame.draw.line(win, colorAleatorio(), (400 + n, 400), (400, 800 - n), 1)
 
+    pygame.display.update()
+
     run = True
 
-    reloj = pygame.time
+    reloj = pygame.time.Clock()
 
     # Lógica de pygame que básicamente es la función main pero en tiempo real
     while run:
@@ -96,8 +98,6 @@ def dibujarB():
                 run = False
                 pygame.quit()
                 main()
-
-    pygame.display.update()
 
 
 # Toma del punto anterior y avanza 2.5 más que la línea pasada perpendicularmente girando 90 grados
@@ -145,21 +145,21 @@ def dibujarC():
 
         i = i + 1
 
-        run = True
+    pygame.display.update()
 
-        reloj = pygame.time
+    run = True
 
-        # Lógica de pygame que básicamente es la función main pero en tiempo real
-        while run:
-            reloj.tick(4)  # Para salvar al planeta
+    reloj = pygame.time.Clock()
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    pygame.quit()
-                    main()
+    # Lógica de pygame que básicamente es la función main pero en tiempo real
+    while run:
+        reloj.tick(4)  # Para salvar al planeta
 
-        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                main()
 
 
 # En las aristas de un dodecágono de radio 150, dibuja 12 círculos
@@ -179,9 +179,11 @@ def dibujarD():
     for n in range(12):
         pygame.draw.circle(win, negro, (400 + int(150*cos(radians(-30*(n+1)))), 400 + int(150*sin(radians(-30*(n+1))))), 150, 1)
 
+    pygame.display.update()
+
     run = True
 
-    reloj = pygame.time
+    reloj = pygame.time.Clock()
 
     # Lógica de pygame que básicamente es la función main pero en tiempo real
     while run:
@@ -192,8 +194,6 @@ def dibujarD():
                 run = False
                 pygame.quit()
                 main()
-
-    pygame.display.update()
 
 
 # Usando fórmula dada por el profesor, se aproxima al valor de PI
@@ -220,7 +220,8 @@ def hacerPiramidesDeNumeros():
     for i in range(9):
         print("%d * 8 + %d = %d" % (ultimoValor*10+i+1, i+1, ((ultimoValor*10)+i+1)*8+i+1))
         ultimoValor = ultimoValor * 10 + i + 1
-
+    print("""
+    """)
     ultimoValor = 0
     for n in range(9):
         print("%d * %d = %d" % (ultimoValor * 10 + 1, ultimoValor * 10 + 1, (ultimoValor * 10 + 1)**2))
@@ -264,66 +265,3 @@ Mision 5: Seleccione que quiere hacer:
 
 
 main()
-
-"""# Toma inputs del teclado y llama función pertinente
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_0]:
-        run = False
-
-    if keys[pygame.K_1]:
-        win.fill(blanco)
-        dibujarA()
-
-    if keys[pygame.K_2]:
-        win.fill(blanco)
-        dibujarB()
-
-    if keys[pygame.K_3]:
-        win.fill(blanco)
-        dibujarC()
-
-    if keys[pygame.K_4]:
-        win.fill(blanco)
-        dibujarD()
-
-    if keys[pygame.K_5]:
-        win.fill(blanco)
-        calcularPi()
-
-    if keys[pygame.K_6]:
-        win.fill(blanco)
-        comprobarNumerosDeTresDigitos()
-
-    if keys[pygame.K_7]:
-        win.fill(blanco)
-        hacerPiramidesDeNumeros()
-
-    if keys[pygame.K_8]:
-        modoPeda = True
-
-    pygame.font.init()
-    myfont = pygame.font.SysFont('Helvetica', 20, True)
-    textsurface = myfont.render("Mision 5: Seleccione que quiere hacer:", False, darColorATexto(modoPeda))
-    textsurface1 = myfont.render("1. Dibujar cuadrados y circulos ", False, darColorATexto(modoPeda))
-    textsurface2 = myfont.render("2. Dibujar parabolas", False, darColorATexto(modoPeda))
-    textsurface3 = myfont.render("3. Dibujar Espiral", False, darColorATexto(modoPeda))
-    textsurface4 = myfont.render("4. Dibujar circulos", False, darColorATexto(modoPeda))
-    textsurface5 = myfont.render("5. Aproximar PI", False, darColorATexto(modoPeda))
-    textsurface6 = myfont.render("6. Contar divisibles entre 19", False, darColorATexto(modoPeda))
-    textsurface7 = myfont.render("7. Imprimir piramides de numeros", False, darColorATexto(modoPeda))
-    textsurface10 = myfont.render("8. Sorpresa para que me de mis 100hp", False, darColorATexto(modoPeda))
-    textsurface8 = myfont.render("0. Salir", False, darColorATexto(modoPeda))
-    textsurface9 = myfont.render("-Que desea hacer?", False, darColorATexto(modoPeda))
-
-    win.blit(textsurface, (0, 0))
-    win.blit(textsurface1, (0, 20))
-    win.blit(textsurface2, (0, 40))
-    win.blit(textsurface3, (0, 60))
-    win.blit(textsurface4, (0, 80))
-    win.blit(textsurface5, (0, 100))
-    win.blit(textsurface6, (0, 120))
-    win.blit(textsurface7, (0, 140))
-    win.blit(textsurface10, (0, 160))
-    win.blit(textsurface8, (0, 180))
-    win.blit(textsurface9, (0, 200))"""
